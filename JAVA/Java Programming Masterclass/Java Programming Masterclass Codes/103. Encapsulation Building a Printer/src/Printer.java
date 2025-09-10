3b0c5d38e2c06a47af4ab5135ff21f8b7813bdd3
Yash Verma <yashverma1630@gmail.com>
2025-09-10 16:57:45 +0530

    More Codes of Encapsulation is Added

--- JAVA/Java Programming Masterclass/Java Programming Masterclass Codes/103. Encapsulation Building a Printer/src/Printer.java
+++ JAVA/Java Programming Masterclass/Java Programming Masterclass Codes/103. Encapsulation Building a Printer/src/Printer.java
@@ -7,14 +7,14 @@ public class Printer {
 
     public Printer(int tonerLevel, boolean duplex) {
         this.pagesPrinted = 0;
-        this.tonerLevel = (tonerLevel >= 0 && tonerLevel <=100)?tonerLevel:-1;
-        this.duplex = duplex;
+        this.tonerLevel = (tonerLevel >= 0 && tonerLevel <=100)?tonerLevel:-1; // ternary operator checks tonerLevel validation
+        this.duplex = duplex; // duplex true means pages will be printed both the sides. and false means pages will be printed single sided.
     }
 
     public int addToner(int tonerAmount){
 
         int tempAmount = tonerLevel + tonerAmount;
-        if(tempAmount > 100 || tempAmount < 0){
+        if(tempAmount > 100 || tempAmount < 0){ // checks if after adding toner into the machine will be under limits.
             return -1;
         }
         tonerLevel += tonerAmount;
@@ -22,12 +22,12 @@ public class Printer {
     }
 
     public int printPages(int pages) {
-        int jobPages = (duplex)?(pages/2)+(pages%2):pages;
-        pagesPrinted += jobPages;
+        int jobPages = (duplex)?(pages/2)+(pages%2):pages; //checks if pages to be printed both the sides or single side.
+        pagesPrinted += jobPages; // adds total pages used to pagesPrinted.
         return jobPages;
     }
 
     public int getPagesPrinted() {
-        return pagesPrinted;
+        return pagesPrinted; // simply returns the total no. of pages used.
     }
 }
